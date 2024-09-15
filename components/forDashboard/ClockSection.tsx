3c89fddc8a5459e3e8e8055e12d0545c563634e7
aLearningLad <mocmanca@gmail.com>
2024-09-15 22:59:44 +0200

    starting right side

---
+++ components/forDashboard/ClockSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ClockSection = () => {
+  return (
+    <div className=" w-full h-52 bg-neutral-300/20 p-2 rounded-lg">
+      ClockSection
+    </div>
+  );
+};
+
+export default ClockSection;
--- components/forDashboard/RightSide.tsx
+++ components/forDashboard/RightSide.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import ClockSection from "./ClockSection";
 
 const RightSide = () => {
   return (
-    <div className="w-full lg:h-full min-h-[70vh] lg:w-7/12 border-2 border-pink-400">
-      RightSide
+    <div className="w-full p-1 md:p-5 lg:p-7 lg:h-full min-h-[70vh] flex flex-col gap-7 lg:w-7/12 border-2 border-pink-400">
+      {/* clock */}
+      <ClockSection />
+
+      {/* schedule & reminder list */}
     </div>
   );
 };
