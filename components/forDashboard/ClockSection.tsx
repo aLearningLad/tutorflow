f9c0fbf0ceac1665514c89b6de86bea79afe2b53
aLearningLad <mocmanca@gmail.com>
2024-09-16 20:51:26 +0200

    clock section and assets

--- components/forDashboard/ClockSection.tsx
+++ components/forDashboard/ClockSection.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 const ClockSection = () => {
   return (
-    <div className=" w-full h-52 bg-neutral-300/20 p-2 rounded-lg">
-      ClockSection
+    <div className=" w-full h-52 bg-neutral-300/20 p-2 rounded-lg relative">
+      <div className=" absolute bottom-0 right-0">
+        {/* plant image  */}
+        <Image src="/assets/water.png" width={150} height={150} alt="plant" />
+      </div>
     </div>
   );
 };
