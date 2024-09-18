327485a169ff37627fba21b41045157857755f6d
aLearningLad <mocmanca@gmail.com>
2024-09-18 23:26:30 +0200

    enter session logic skeleton

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,7 +5,7 @@ const SignInPage = () => {
   return (
     <main className=" min-h-screen flex justify-center items-center bg-black text-white flex-col">
       <h2 className=" text-white">Hi there!</h2>
-      <div>
+      <div className=" w-full h-60 border-4 border-white rounded-lg">
         <SignIn />
       </div>
     </main>
