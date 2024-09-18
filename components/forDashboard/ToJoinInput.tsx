f079f4e88228e2823c377419c5f19c4c1c3c4553
aLearningLad <mocmanca@gmail.com>
2024-09-18 22:35:45 +0200

    meeting join started

--- components/forDashboard/ToJoinInput.tsx
+++ components/forDashboard/ToJoinInput.tsx
@@ -1,11 +1,28 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const ToJoinInput = () => {
   const [meetingLink, setMeetingLink] = useState<string>("");
+  const router = useRouter();
 
-  return <div className=" w-full md:w-8/12 lg:w-6/12 flex flex-col"></div>;
+  const enterSession = async () => {
+    try {
+    } catch (error) {}
+  };
+
+  return (
+    <div className=" w-full md:w-8/12 lg:w-6/12 flex flex-col">
+      <input
+        type="text"
+        placeholder="Eg. http://thetutsession.com/2%41./(6^fhdbe"
+        value={meetingLink}
+        onChange={(e) => setMeetingLink(e.target.value)}
+      />
+      <button>Go to meeting</button>
+    </div>
+  );
 };
 
 export default ToJoinInput;
