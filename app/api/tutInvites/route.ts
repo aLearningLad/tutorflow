698d5aab622e24718d5dcf62e3cd9bd106059d04
aLearningLad <mocmanca@gmail.com>
2024-09-20 23:31:31 +0200

    nodemailer api call fleshing out

--- app/api/tutInvites/route.ts
+++ app/api/tutInvites/route.ts
@@ -3,4 +3,13 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const { inviteeList, sessionLink } = await req.json();
+
+  if (!inviteeList || !sessionLink) {
+    return NextResponse.json(
+      {
+        mmessage: "Either an invitee list or session link is missing",
+      },
+      { status: 400 }
+    );
+  }
 }
