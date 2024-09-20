c05992931b24da429d6d44557f8c161a8bb634f0
aLearningLad <mocmanca@gmail.com>
2024-09-20 23:56:09 +0200

    server call logic started. Yet to do frontend call

--- app/api/tutInvites/route.ts
+++ app/api/tutInvites/route.ts
@@ -14,20 +14,30 @@ export async function POST(req: Request) {
     );
   }
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-  });
-
-  for (const invitee of inviteeList) {
-    await transporter.sendMail({
-      from: process.env.EMAIL,
-      to: invitee,
-      subject: "Thato is keen to have you join his tut session!",
-      text: `You've been invited to an invite-only tutorial session. Join here: ${sessionLink}`,
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
     });
+
+    for (const invitee of inviteeList) {
+      await transporter.sendMail({
+        from: process.env.EMAIL,
+        to: invitee,
+        subject: "Thato is keen to have you join his tut session!",
+        text: `You've been invited to an invite-only tutorial session. Join here: ${sessionLink}`,
+      });
+    }
+
+    return NextResponse.json({ message: "Invites have been sent!" });
+  } catch (error) {
+    console.log("Error while trying to send email invites: ", error);
+    return NextResponse.json(
+      { message: "Error sending email invites" },
+      { status: 500 }
+    );
   }
 }
