96b160e6f3fbb88799cc64003cad15b7609e7cbc
aLearningLad <mocmanca@gmail.com>
2024-09-20 21:01:41 +0200

    invite api progress

--- app/api/tutInvites/route.ts
+++ app/api/tutInvites/route.ts
@@ -1,4 +1,6 @@
 import nodemailer from "nodemailer";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {}
+export async function POST(req: Request) {
+  const { inviteeList, sessionLink } = await req.json();
+}
