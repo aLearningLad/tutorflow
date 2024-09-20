d73711746e844c57300dcab2d940dd26ddadd006
aLearningLad <mocmanca@gmail.com>
2024-09-20 23:48:56 +0200

    tinkering with nodemailer transporter options

--- app/api/tutInvites/route.ts
+++ app/api/tutInvites/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const { inviteeList, sessionLink } = await req.json();
 
+  // handle error if either is missing
   if (!inviteeList || !sessionLink) {
     return NextResponse.json(
       {
@@ -12,4 +13,12 @@ export async function POST(req: Request) {
       { status: 400 }
     );
   }
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
 }
