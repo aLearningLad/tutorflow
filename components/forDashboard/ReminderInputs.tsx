2c6d35a8ea907232e175b230b76a6d7c7dc89491
aLearningLad <mocmanca@gmail.com>
2024-09-23 09:45:06 +0200

    reminder inputs update

--- components/forDashboard/ReminderInputs.tsx
+++ components/forDashboard/ReminderInputs.tsx
@@ -1,7 +1,45 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 const ReminderInputs = () => {
-  return <div>ReminderInputs</div>;
+  // to get author email
+  useEffect(() => {
+    const getAuthor = async () => {
+      alert("Author getting function called!");
+    };
+
+    getAuthor();
+  }, []);
+
+  const [reminderDetails, setReminderDetails] = useState<TreminderCard>({
+    author: "", // get from clerk
+    reminderId: "", // call nanoid here
+    title: "", // get from user input here
+    startsAt: "", // get from user input here
+    endsAt: "", // get from user input here
+    detail: "", // get from user input here
+    shareableLink: "", // generate by attaching using nanoid to create roomId
+    is_private: false, // allow user to change this via selector
+  });
+
+  return (
+    <div className="w-full h-[70vh] border-4 border-white flex flex-col relative gap-4">
+      {/* inputs ===> scrollable */}
+      <div className="w-full lg:h-[90%] bg-pink-400/40">
+        {/* inputs needed are from type IreminderCard
+      
+      */}
+      </div>
+
+      {/* submit button  */}
+      <div className=" flex justify-center items-center w-full lg:h-[10%] ">
+        <button className=" w-full h-full bg-orange-400 text-white text-lg rounded-md ">
+          Save reminder
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ReminderInputs;
