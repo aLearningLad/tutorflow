722a3556157377674efaca7d01d4b8013a0beb14
aLearningLad <mocmanca@gmail.com>
2024-09-23 19:43:13 +0200

    progress with reminder cards

--- components/forDashboard/ReminderList.tsx
+++ components/forDashboard/ReminderList.tsx
@@ -1,24 +1,39 @@
 import { createClient } from "@/lib/supabase/server";
-import { useUser } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
 const ReminderList = async () => {
   const supabase = createClient();
 
-  const userEmail = useUser().user?.emailAddresses[0].emailAddress;
+  const userDetails = await currentUser();
+
+  const email = userDetails?.emailAddresses[0].emailAddress;
 
   const { data: allReminders, error: remindersError } = await supabase
     .from("reminders")
     .select("*")
-    .eq("author_email", userEmail);
+    .eq("author_email", email);
+  console.log("These are the reminders: ", allReminders);
 
-  const reminders = [1, 2, 3, 4, 5, 6];
+  if (allReminders && allReminders.length > 0) {
+    const reminders = [1, 2, 3, 4, 5, 6];
+    return (
+      <div className=" w-full h-[50vh] overflow-auto flex flex-col gap-5 lg:gap-7">
+        {allReminders.map((reminder) => (
+          <div className=" w-full min-h-60 rounded-lg bg-slate-700 p-5 flex flex-col">
+            This is a card
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
-    <div className=" w-full h-[50vh] overflow-auto flex flex-col gap-5 lg:gap-7">
-      {reminders.map((reminder) => (
-        <div className=" w-full min-h-60 rounded-lg bg-slate-700 p-5 flex flex-col">
-          This is a card
-        </div>
-      ))}
+    <div className="w-full h-[50vh] items-center justify-center text-center flex flex-col gap-5 lg:gap-7">
+      <h1>No reminders stored yet</h1>
+      <p>
+        Create some to keep track of important tasks or discussions to be
+        addressed within your tutorials
+      </p>
     </div>
   );
 };
