2fe7553d53c0213b92f86ccb252c82a82f0d970f
aLearningLad <mocmanca@gmail.com>
2024-09-23 18:56:46 +0200

    reminder creation complete

--- components/forDashboard/ReminderInputs.tsx
+++ components/forDashboard/ReminderInputs.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/lib/supabase/client";
 import { useUser } from "@clerk/nextjs";
 import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
+import { DialogTrigger } from "../ui/dialog";
 
 const ReminderInputs = () => {
   // to get author email
@@ -81,82 +82,98 @@ const ReminderInputs = () => {
     }
   };
 
-  return (
-    <div className="w-full h-[70vh] border-4 border-white flex flex-col relative gap-4">
-      {/* inputs ===> scrollable */}
-      <div className="w-full overflow-auto lg:h-[90%] bg-pink-400/40 gap-y-4 flex flex-col items-center justify-start ">
-        <section className="w-full min-h-24 flex flex-col items-center text-center justify-center">
-          <label className=" text-[18px] " htmlFor="title">
-            Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            placeholder="Call Mr. Anderson"
-            className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
-            onChange={handleDetailsChange}
-          />
-        </section>
-        <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center justify-center">
-          <label className=" text-[18px] " htmlFor="startsAt">
-            Starts at
-          </label>
-          <input
-            type="text"
-            name="startsAt"
-            placeholder="Eg. 18h00"
-            className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
-            onChange={handleDetailsChange}
-          />
-        </section>
-        <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center justify-center">
-          <label className=" text-[18px] " htmlFor="endsAt">
-            Ends at
-          </label>
-          <input
-            type="text"
-            name="endsAt"
-            placeholder="Eg. 19h30"
-            className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
-            onChange={handleDetailsChange}
-          />
-        </section>
-        <section className="w-full min-h-[40vh] lg:min-h-[45vh] border-2 border-white flex flex-col items-center text-center justify-center">
-          <label className=" text-[18px] " htmlFor="detail">
-            Reminder details
-          </label>
-          <textarea
-            name="detail"
-            placeholder="Add more information here"
-            className=" bg-slate-600 h-[90%] lg:h-[85%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
-            onChange={handleDetailsChange}
-          />
-        </section>
-        <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center justify-center">
-          <label className=" text-[18px] " htmlFor="shareableLink">
-            Attach a link
-          </label>
-          <input
-            type="text"
-            name="shareableLink"
-            placeholder="Eg. 19h30"
-            className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
-            onChange={handleDetailsChange}
-          />
-        </section>
-      </div>
+  if (!isSubmitted) {
+    return (
+      <div className="w-full h-[70vh] border-4 border-white flex flex-col relative gap-4">
+        {/* inputs ===> scrollable */}
+        <div className="w-full overflow-auto lg:h-[90%] bg-pink-400/40 gap-y-4 flex flex-col items-center justify-start ">
+          <section className="w-full min-h-24 flex flex-col items-center text-center justify-center">
+            <label className=" text-[18px] " htmlFor="title">
+              Title
+            </label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Eg. call Mr. Anderson"
+              className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
+              onChange={handleDetailsChange}
+            />
+          </section>
+          <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center justify-center">
+            <label className=" text-[18px] " htmlFor="startsAt">
+              Starts at
+            </label>
+            <input
+              type="text"
+              name="startsAt"
+              placeholder="Eg. 18h00"
+              className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
+              onChange={handleDetailsChange}
+            />
+          </section>
+          <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center justify-center">
+            <label className=" text-[18px] " htmlFor="endsAt">
+              Ends at
+            </label>
+            <input
+              type="text"
+              name="endsAt"
+              placeholder="Eg. 19h30"
+              className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
+              onChange={handleDetailsChange}
+            />
+          </section>
+          <section className="w-full min-h-[40vh] lg:min-h-[45vh] border-2 border-white flex flex-col items-center text-center justify-center">
+            <label className=" text-[18px] " htmlFor="detail">
+              Reminder details
+            </label>
+            <textarea
+              name="detail"
+              placeholder="Add more information here"
+              className=" bg-slate-600 h-[90%] lg:h-[85%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
+              onChange={handleDetailsChange}
+            />
+          </section>
+          <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center justify-center">
+            <label className=" text-[18px] " htmlFor="shareableLink">
+              Attach a link
+            </label>
+            <input
+              type="text"
+              name="shareableLink"
+              placeholder="Eg. 19h30"
+              className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
+              onChange={handleDetailsChange}
+            />
+          </section>
+        </div>
 
-      {/* submit button  */}
-      <div className=" flex justify-center items-center w-full lg:h-[10%] ">
-        <button
-          onClick={submitReminder}
-          className=" w-full h-full bg-orange-400 text-white text-lg rounded-md "
-        >
-          Save reminder
-        </button>
+        {/* submit button  */}
+        <div className=" flex justify-center items-center w-full lg:h-[10%] ">
+          <button
+            onClick={submitReminder}
+            className=" w-full h-full bg-orange-400 text-white text-lg rounded-md "
+          >
+            Save reminder
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  if (isSubmitted) {
+    return (
+      <div className="w-full h-fit flex flex-col items-center text-center relative gap-4">
+        <h1 className=" text-xl font-semibold text-white ">Submitted!</h1>
+        <h3 className=" text-[14px] text-neutral-200 ">
+          You may now close this modal
+        </h3>
+        <DialogTrigger className="mt-3 bg-cyan-500 hover:bg-orange-400 transition-all duration-300 ease-in text-white text-lg w-full h-12 rounded-md flex justify-center items-center">
+          Close
+        </DialogTrigger>
+      </div>
+    );
+  }
 };
 
 export default ReminderInputs;
