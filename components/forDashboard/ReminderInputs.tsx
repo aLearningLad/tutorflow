345f53c4b25d5203e26e10767a3ce8eba8c96d6e
aLearningLad <mocmanca@gmail.com>
2024-09-23 10:25:11 +0200

    more changes

--- components/forDashboard/ReminderInputs.tsx
+++ components/forDashboard/ReminderInputs.tsx
@@ -23,21 +23,73 @@ const ReminderInputs = () => {
     is_private: false, // allow user to change this via selector
   });
 
-  const handleDetailsChange = () => {};
+  const handleDetailsChange = (e: any) => {
+    setReminderDetails((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   return (
     <div className="w-full h-[70vh] border-4 border-white flex flex-col relative gap-4">
       {/* inputs ===> scrollable */}
       <div className="w-full overflow-auto lg:h-[90%] bg-pink-400/40 gap-y-4 flex flex-col justify-center">
-        <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center gap-1">
-          <label className=" text-[14px] " htmlFor="title">
+        <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center justify-center">
+          <label className=" text-[18px] " htmlFor="title">
             Title
           </label>
           <input
             type="text"
             name="title"
             placeholder="Call Mr. Anderson"
-            className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full "
+            className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
+            onChange={handleDetailsChange}
+          />
+        </section>
+        <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center justify-center">
+          <label className=" text-[18px] " htmlFor="startsAt">
+            Title
+          </label>
+          <input
+            type="text"
+            name="startsAt"
+            placeholder="Eg. 18h00"
+            className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
+            onChange={handleDetailsChange}
+          />
+        </section>
+        <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center justify-center">
+          <label className=" text-[18px] " htmlFor="endsAt">
+            Title
+          </label>
+          <input
+            type="text"
+            name="endsAt"
+            placeholder="Eg. 19h30"
+            className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
+            onChange={handleDetailsChange}
+          />
+        </section>
+        <section className="w-full min-h-[40vh] lg:min-h-[45vh] border-2 border-white flex flex-col items-center text-center justify-center">
+          <label className=" text-[18px] " htmlFor="detail">
+            Reminder details
+          </label>
+          <textarea
+            name="detail"
+            placeholder="Add more information here"
+            className=" bg-slate-600 h-[90%] lg:h-[85%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
+            onChange={handleDetailsChange}
+          />
+        </section>
+        <section className="w-full min-h-24 border-2 border-white flex flex-col items-center text-center justify-center">
+          <label className=" text-[18px] " htmlFor="shareableLink">
+            Title
+          </label>
+          <input
+            type="text"
+            name="shareableLink"
+            placeholder="Eg. https://tutorflow/tutroom/room-id-comes-here"
+            className=" bg-slate-600 h-[70%] lg:h-[60%] rounded-md text-white px-2 py-1 w-full focus:outline-none focus:bg-black focus:scale-95 transition duration-300 ease-in"
             onChange={handleDetailsChange}
           />
         </section>
