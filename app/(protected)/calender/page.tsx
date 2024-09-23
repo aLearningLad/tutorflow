137292fd6f9882ab8e3702ed71ce06654dfe5d25
aLearningLad <mocmanca@gmail.com>
2024-09-23 23:30:06 +0200

    calender page skeleton

---
+++ app/(protected)/calender/page.tsx
@@ -0,0 +1,12 @@
+import Notifs from "@/components/forDashboard/Notifs";
+import React from "react";
+
+const CalenderPage = () => {
+  return (
+    <main className="min-h-screen bg-slate-900  flex flex-col text-white">
+      <Notifs />
+    </main>
+  );
+};
+
+export default CalenderPage;
