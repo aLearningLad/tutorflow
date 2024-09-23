bbd014abbfdec12bbb5636d4815129bed20c3379
aLearningLad <mocmanca@gmail.com>
2024-09-23 23:12:03 +0200

    additions to reminder btn

--- components/forDashboard/DeleteReminderBtn.tsx
+++ components/forDashboard/DeleteReminderBtn.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { FaTrashAlt } from "react-icons/fa";
+
 const DeleteReminderBtn: React.FC<Tdeletereminderbtn> = ({ reminderId }) => {
-  return <div>DeleteReminderBtn</div>;
+  const handleDelete = async () => {};
+
+  return (
+    <button className="w-fit px-3 group bg-white flex justify-center items-center h-[70%] rounded-md text-center py-2">
+      <FaTrashAlt size={18} className=" text-red-600 group-hover:text-black" />
+    </button>
+  );
 };
 
 export default DeleteReminderBtn;
--- components/forDashboard/ReminderList.tsx
+++ components/forDashboard/ReminderList.tsx
@@ -3,6 +3,7 @@ import { auth, currentUser } from "@clerk/nextjs/server";
 import ReminderBtn from "./ReminderBtn";
 import { SlOptions } from "react-icons/sl";
 import DeleteReminderBtn from "./DeleteReminderBtn";
+import { FaLink } from "react-icons/fa";
 
 const ReminderList = async () => {
   const supabase = createClient();
@@ -20,19 +21,44 @@ const ReminderList = async () => {
   if (allReminders && allReminders.length > 0) {
     return (
       <div className=" w-full h-[50vh] overflow-auto flex flex-col gap-5 lg:gap-7">
-        {allReminders.map((reminder: TreminderCard) => (
+        {allReminders.map((reminder) => (
           <div className=" w-full min-h-60 rounded-lg bg-slate-700 p-5 flex flex-col px-1 md:px-2 lg:px-3 py-2 lg:py-3">
             {/* top section */}
-            <section className=" w-full flex justify-between items-center h-[20%]">
-              <h3>{reminder.title}</h3>
+            <section className=" w-full flex justify-between items-center h-[20%] border-b-2 border-slate-600">
+              <h3 className=" text-2xl text-white text-left ">
+                {reminder.title}
+              </h3>
               <DeleteReminderBtn reminderId={reminder.reminderId} />
             </section>
 
             {/* middle section */}
-            <section className=" w-full flex text-left  "></section>
+            <section className=" w-full flex text-left h-[60%] ">
+              <p className=" text-[16px] text-neutral-200 ">
+                {reminder.detail}
+              </p>
+            </section>
 
             {/* lower section */}
-            <section className=" w-full flex justify-between "></section>
+            <section className=" w-full flex flex-col lg:flex-row justify-between h-[20%] ">
+              <span className=" w-full lg:w-1/2 flex justify-start gap-1">
+                <p className=" text-[14px] text-neutral-200">
+                  {reminder.startsat}
+                </p>
+                <p>until</p>
+                <p className=" text-[14px] text-neutral-200">
+                  {reminder.endsat}
+                </p>
+              </span>
+
+              <span className=" w-full lg:w-1/2 flex justify-end gap-1 px-3 ">
+                <button className=" w-32 flex h-full bg-blue-600 rounded-md justify-center items-center gap-1 ">
+                  <FaLink size={14} />
+                  <p className=" w-9/12 h-full overflow-clip flex text-[14px] rounded-md text-ellipsis items-center justify-center ">
+                    {reminder.shareable_link}
+                  </p>
+                </button>
+              </span>
+            </section>
           </div>
         ))}
       </div>
