ae99983eaac17b9e8142d7147c167d5a8dd7cb2e
aLearningLad <mocmanca@gmail.com>
2024-09-24 03:31:47 +0200

    calender page progress

--- app/(protected)/calender/page.tsx
+++ app/(protected)/calender/page.tsx
@@ -3,8 +3,11 @@ import React from "react";
 
 const CalenderPage = () => {
   return (
-    <main className="min-h-screen bg-slate-900  flex flex-col text-white">
+    <main className="min-h-screen bg-slate-900 flex flex-col text-white">
       <Notifs />
+      <div className=" h-[90vh] flex flex-col p-1 md:p-2 lg:p-5 items-center text-center ">
+        <section className=" rounded-lg w-full h-full bg-neutral-300/70 grid grid-cols-1 lg:grid-cols-2 "></section>
+      </div>
     </main>
   );
 };
