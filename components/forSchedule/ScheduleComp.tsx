2c5ba230e7f9b78bd5ee3346b727d0c5cf278a20
aLearningLad <mocmanca@gmail.com>
2024-09-24 22:26:21 +0200

    schedule comp more progress

--- components/forSchedule/ScheduleComp.tsx
+++ components/forSchedule/ScheduleComp.tsx
@@ -1,6 +1,5 @@
 import { createClient } from "@/lib/supabase/server";
 import { currentUser } from "@clerk/nextjs/server";
-import React from "react";
 
 const ScheduleComp = async () => {
   const supabase = createClient();
@@ -20,9 +19,14 @@ const ScheduleComp = async () => {
   }
 
   return (
-    <div>
-      Nothing to show yet. Would you like to create a new reminder to add to
-      your schedule?
+    <div className=" w-full h-[70vh] border-4 border-white flex justify-center items-center flex-col text-center">
+      <h2 className=" text-xl text-white ">
+        Nothing to show yet. Would you like to create a new reminder to add to
+        your schedule?
+      </h2>
+      <button className=" text-lg bg-orange-500 text-white px-4 py-2 rounded-md ">
+        Add new reminder
+      </button>
     </div>
   );
 };
