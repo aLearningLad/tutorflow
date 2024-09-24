8c6e4ebd6aaf52d220859a6dc0286e3daf2f3981
aLearningLad <mocmanca@gmail.com>
2024-09-24 20:34:00 +0200

    additions to calender

--- app/(protected)/calender/page.tsx
+++ app/(protected)/calender/page.tsx
@@ -20,7 +20,16 @@ const CalenderPage = async () => {
       <main className="min-h-screen bg-slate-900 flex flex-col text-white">
         <Notifs />
         <div className="h-[90vh] flex flex-col p-1 md:p-2 lg:p-5 items-center text-center ">
-          <section className="rounded-lg w-full h-full bg-neutral-300/70 grid grid-cols-1 lg:grid-cols-2 "></section>
+          <section className="rounded-lg gap-4 lg:gap-12 p-2 md:p-5 lg:p-9 w-full h-full bg-neutral-300/70 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 overflow-auto ">
+            {calenderTutData.map((tut) => (
+              <div className=" min-h-[20vh] max-h-[30vh] px-1 lg:px-2 w-full border-4 border-cyan-500 rounded-lg flex flex-col items-center justify-between text-center">
+                <span className=" w-full flex justify-between items-center ">
+                  <p>{tut.date_of_tut}</p>
+                  <p>{tut.start_time}</p>
+                </span>
+              </div>
+            ))}
+          </section>
         </div>
       </main>
     );
