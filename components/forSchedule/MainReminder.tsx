af2bbe3f6a205c2775930d4d41cd2b61546ab1a8
aLearningLad <mocmanca@gmail.com>
2024-09-25 08:38:06 +0200

    main reminder comp additions

--- components/forSchedule/MainReminder.tsx
+++ components/forSchedule/MainReminder.tsx
@@ -19,16 +19,19 @@ const MainReminder: React.FC<TreminderCard> = ({
           image comes here
         </div>
       </section>
-      <section className=" w-full lg:w-5/12 h-full flex flex-col bg-orange-400 p-1 md:p-2 lg:p-5 rounded-lg">
+      <section className=" w-full lg:w-5/12 h-full flex flex-col bg-blue-600 p-1 md:p-2 lg:p-5 rounded-lg">
         {/* right side */}
         <section className=" w-full py-2">
           <div className=" w-full  flex justify-between ">
-            <span>
-              <p>from</p>
+            <span className="p-2 bg-neutral-100/30 rounded-md">
+              <p className="text-[10px] ">from</p>
               <h2>{startsAt}</h2>
             </span>
-            <span>
-              <p>until</p>
+            <span className="min-w-[30%] max-w-[50%] flex items-center justify-center">
+              <p className=" text-xl  overflow-auto flex ">{title}</p>
+            </span>
+            <span className=" p-2 bg-neutral-100/30 rounded-md ">
+              <p className="text-[10px]">until</p>
               <h2>{endsAt}</h2>
             </span>
           </div>
@@ -45,6 +48,9 @@ const MainReminder: React.FC<TreminderCard> = ({
             <p className=" text-white text-[12px] ">by</p>
             <p className=" text-[18px] ">{author}</p>
           </span>
+        </section>
+        <section className=" w-full overflow-auto rounded-md text-ellipsis bg-slate-100/30 py-2 px-4 ">
+          <p>{detail}</p>
         </section>
       </section>
     </div>
