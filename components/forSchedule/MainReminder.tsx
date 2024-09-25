3494ddb87308d012b924457da766e0e61c9d9b46
aLearningLad <mocmanca@gmail.com>
2024-09-25 08:29:42 +0200

    main reminder additions

--- components/forSchedule/MainReminder.tsx
+++ components/forSchedule/MainReminder.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const MainReminder: React.FC<TreminderCard> = ({
@@ -18,18 +19,33 @@ const MainReminder: React.FC<TreminderCard> = ({
           image comes here
         </div>
       </section>
-      <section className=" w-full lg:w-5/12 h-full flex bg-red-600/30 p-1 md:p-2 lg:p-5 rounded-lg">
+      <section className=" w-full lg:w-5/12 h-full flex flex-col bg-orange-400 p-1 md:p-2 lg:p-5 rounded-lg">
         {/* right side */}
-        <div className=" w-full py-2 flex justify-between ">
-          <span>
-            <p>from</p>
-            <h2>{startsAt}</h2>
+        <section className=" w-full py-2">
+          <div className=" w-full  flex justify-between ">
+            <span>
+              <p>from</p>
+              <h2>{startsAt}</h2>
+            </span>
+            <span>
+              <p>until</p>
+              <h2>{endsAt}</h2>
+            </span>
+          </div>
+        </section>
+        <section className=" w-full flex justify-center items-center text-center py-5 flex-col ">
+          <Image
+            alt="img"
+            src={"/assets/speaker3.png"}
+            width={100}
+            height={80}
+            className=" p-1 bg-cyan-500 rounded-md"
+          />
+          <span className=" w-full flex items-center justify-center gap-2">
+            <p className=" text-white text-[12px] ">by</p>
+            <p className=" text-[18px] ">{author}</p>
           </span>
-          <span>
-            <p>until</p>
-            <h2>{endsAt}</h2>
-          </span>
-        </div>
+        </section>
       </section>
     </div>
   );
