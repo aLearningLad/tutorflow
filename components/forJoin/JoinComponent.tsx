dd732bd301c60ad3fb6a855786941af39166165c
aLearningLad <mocmanca@gmail.com>
2024-09-25 21:22:06 +0200

    join component additions

--- components/forJoin/JoinComponent.tsx
+++ components/forJoin/JoinComponent.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+"use client";
+
+import { FaLink } from "react-icons/fa";
 
 const JoinComponent = () => {
+  // later additions
+  // add functionality so that when user pastes link, a check is done to
+  // confirm via database status that the session is in-fact live/ongoing
+  // prompt user to wait a bit if the session is not yet live.
+  // NB ===> requires new column in sessions table, called "is_live", will be boolean
+  // and determine whether attendees can join it
+
   return (
-    <div className=" w-full min-h-[90vh] border-4 border-white flex justify-center items-center text-white ">
-      JoinComponent
+    <div className=" w-full min-h-[90vh] border-4 flex-col border-white flex justify-center items-center text-white ">
+      <div className=" w-full flex flex-col items-center justify-center text-center mb-2 lg:mb-5">
+        <h1 className=" text-2xl">Paste your tutorial session link below</h1>
+        <p>You will be directed to the session if it is live</p>
+      </div>
+      <div className=" w-full md:w-10/12 lg:w-8/12 xl:w-1/2 py-5 px-1 md:px-3 lg:px-8 flex justify-center items-center ">
+        <div className=" w-full flex items-center justify-center gap-2 ">
+          <FaLink size={18} className="text-white" />
+          <input
+            type="text"
+            className=" w-full h-12 rounded-md bg-slate-600 focus:outline-none px-4 lg:px-7"
+            placeholder="Eg. https://tutorflow.com/tutroom/tut-session-id-here"
+          />
+        </div>
+      </div>
     </div>
   );
 };
