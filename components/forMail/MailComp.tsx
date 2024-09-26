5d6aede2cdb68496757041fcfae8ee6332e260f7
aLearningLad <mocmanca@gmail.com>
2024-09-26 22:43:14 +0200

    recipient adding logic

--- components/forMail/MailComp.tsx
+++ components/forMail/MailComp.tsx
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+"use client";
+
+import { useState } from "react";
+import RecipientTab from "./RecipientTab";
 
 const MailComp = () => {
   const [emailContents, setEmailContents] = useState<string>("");
-  const [recipientList, setRecipientList] = useState<string>("");
+  const [recipientList, setRecipientList] = useState<string[]>([]);
+
+  const handleAdd = () => {
+    if (emailContents.length < 5) {
+      alert("Email address too short!");
+      return;
+    }
+
+    setRecipientList((prev) => [...prev, emailContents]);
+  };
 
   return (
     <div className=" h-[90vh] w-full border-4 border-white flex flex-col itemsc justify-center ">
@@ -16,12 +28,35 @@ const MailComp = () => {
 
         {/* add recipients */}
         <div className=" w-5/12 h-full flex flex-col justify-center items-center text-center px-1 md:px-5 lg:px-12">
-          <h2>Add mail recipients</h2>
+          <h2 className=" text-xl text-white">Add mail recipients</h2>
           <input
             type="text"
             placeholder="Eg. sallymichaels@gmail.com"
             className=" w-full focus:scale-105 transition-all duration-300 ease-in h-12 py-2 px-3 text-neutral-300 focus:outline-none text-[18px] bg-slate-600 rounded-md"
+            value={emailContents}
+            onChange={(e) => setEmailContents(e.target.value)}
           />
+          <button
+            onClick={handleAdd}
+            className=" w-full h-12 mt-4 hover:bg-blue-600/60 bg-blue-600 text-white rounded-md"
+          >
+            Add
+          </button>
+          <div className=" w-full h-[30%] bg-neutral-50/20 rounded-lg mt-5 ">
+            {/* will contain recipientList */}
+            {recipientList.length > 0 ? (
+              <div className=" p-2 lg:p-3 flex flex-col items-center text-center">
+                {recipientList.map((person) => (
+                  <RecipientTab />
+                ))}
+              </div>
+            ) : (
+              <div className=" p-2 md:p-3 lg:p-5 w-full h-full flex justify-center items-center text-center text-neutral-300 ">
+                Attach email addresses to share your message with. Your
+                recipient list will be displayed here
+              </div>
+            )}
+          </div>
         </div>
       </section>
 
---
+++ components/forMail/RecipientTab.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecipientTab = () => {
+  return <div>RecipientTab</div>;
+};
+
+export default RecipientTab;
