6fb6ad3661a7ec5538827390f521cc765af02767
aLearningLad <mocmanca@gmail.com>
2024-09-28 20:47:37 +0200

    additions to mobile nav bar

--- components/forDashboard/Sidebar.tsx
+++ components/forDashboard/Sidebar.tsx
@@ -1,8 +1,7 @@
 import { Isidebar } from "@/Interfaces";
 import { sidebardata } from "@/misc/sidebardata";
-import Link from "next/link";
-import { NextRequest, NextResponse } from "next/server";
 import SideBarBtn from "../forSidebar/SideBarBtn";
+import { CgMenuGridR } from "react-icons/cg";
 
 const Sidebar: React.FC<Isidebar> = () => {
   return (
@@ -10,7 +9,12 @@ const Sidebar: React.FC<Isidebar> = () => {
       <section className=" w-2/12 lg:w-full hidden lg:flex justify-center items-center h-full lg:h-1/6 xl:h-[13%] bg-neutral-500/40">
         {/* app logo here */}
       </section>
-
+      <section className=" w-full h-full flex justify-center items-center">
+        {/* only on small screens */}
+        <button className=" w-fit p-2 h-fit rounded-md bg-orange-500 active:bg-blue-600 active:scale-95 transition-all duration-300 ease-in">
+          <CgMenuGridR className=" text-white" size={30} />
+        </button>
+      </section>
       <section className="w-full h-full lg:h-5/6 flex flex-row lg:flex-col">
         {/* only on desktop */}
         <ul className=" w-full h-full lg:flex lg:h-[70%] hidden flex-col text-center justify-around items-center lg:px-1 xl:px-2">
@@ -24,10 +28,6 @@ const Sidebar: React.FC<Isidebar> = () => {
             />
           ))}
         </ul>
-      </section>
-
-      <section className=" w-full h-full flex justify-between items-center px-1 md:px-2 ">
-        {/* only on small screens */}
       </section>
     </nav>
   );
