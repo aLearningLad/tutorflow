fa66cda925d03692092aa0640da16015823da354
aLearningLad <mocmanca@gmail.com>
2024-09-29 12:58:44 +0200

    nodemailer reminder api route progress

--- app/api/sendMail.ts/route.ts
+++ app/api/sendMail.ts/route.ts
@@ -1,4 +1,4 @@
-import nodeMailer from "nodemailer";
+import nodemailer from "nodemailer";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -22,4 +22,24 @@ export async function POST(req: Request) {
       message: "Bruv, important info from the client is missing.",
     });
   }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.GOOGLE_APP_PASSWORD,
+      },
+    });
+
+    // send a reminder to each invitee
+    for (const invitee of invited_emails) {
+      await transporter.sendMail({
+        from: process.env.EMAIL,
+        to: invitee,
+        subject: "Tutorial Session Reminder",
+        text: `Hi there! Just a quick reminder that you have a tutorial session with Thato. It is scheduled for ${date_of_tut}, and will commence at ${start_time}`,
+      });
+    }
+  } catch (error) {}
 }
